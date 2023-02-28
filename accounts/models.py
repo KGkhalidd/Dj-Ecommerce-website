887ca456260d4653610278a9b718c1f814b260a4
khaled <khaledgamal1345@gmail.com>
2023-02-28 05:56:09 +0200

    added the authentication

--- accounts/models.py
+++ accounts/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from django_countries.fields import CountryField
 from django.contrib.auth.models import User 
 from utils.generate_code import generate_code
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 
-class profile(models.Model):
+class Profile(models.Model):
     user= models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     image= models.ImageField( upload_to='users')
 
     code=models.CharField(max_length=10 , default= generate_code )
     code_used = models.BooleanField(default= False)
     active = models.BooleanField(default= False)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 DATA_TYPE= (
     ('Home','Home'),
